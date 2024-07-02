313fc2a3a796597e45b81e6ddb2480b804d4d0b4
malinkatetyana <malinkatetyana@gmail.com>
2024-07-02 14:02:28 +0300

    add two projects

---
+++ lab_10_1_oop/lab_10_1_oop/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_10_1_oop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Random random = new Random();
+
+            int sizeMass, i, sizeSearchMass = 0, j = -1;
+            Console.Write("Введіть кількість значень в масиві: ");
+            sizeMass = int.Parse(Console.ReadLine());
+
+            int[] consoleMass = new int[sizeMass], searchMass;
+
+            for (i = 0; i < consoleMass.Length; i++)
+            {
+                consoleMass[i] = random.Next(-10, 10);
+                if (consoleMass[i]>1)
+                {
+                    sizeSearchMass++;
+                }
+            }
+
+            Console.WriteLine();
+
+            Console.Write("Згенерований масив: ");
+            for (i = 0; i < consoleMass.Length; i++)
+            {
+                Console.Write(consoleMass[i] + " ");
+            }
+            Console.WriteLine();
+
+            if (sizeSearchMass == 0)
+            {
+                Console.WriteLine("Згенерований масив немає чисел більше 1");
+            }
+            else
+            {
+                searchMass = new int[sizeSearchMass];
+
+                for (i = 0; i < consoleMass.Length; i++)
+                {
+                    if (consoleMass[i] > 1)
+                    {
+                        j++;
+                        searchMass[j] = consoleMass[i];
+                    }
+                }
+
+                Console.Write("Елементи згенерованого\nмасиву, що більше 1: ");
+
+                for (i = 0; i < searchMass.Length; i++)
+                {
+                    Console.Write(searchMass[i] + " ");
+                }
+            }
+            Console.WriteLine();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ lab_10_2_oop/lab_10_2_oop/Program.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_10_2_oop
+{
+    internal class Program
+    {
+        static int SumElementsMatrAboveMainLine(int[,] matr, int n, int m)
+        {
+            int sumElement = 0, i, j;
+
+            for (i = 0; i < n; i++)
+            {
+                for (j = 0; j < m; j++)
+                {
+                    if (j > i)
+                    {
+                        sumElement += matr[i, j];
+                    }
+                }
+            }
+
+            return sumElement;
+        }
+        static int SearchMinElement(int[,] matr, int n, int m)
+        {
+            int minElement = matr[0, 0], i, j;
+
+            for (i = 0; i < n; i++)
+            {
+                for (j = 0; j < m; j++)
+                {
+                    if (minElement > matr[i, j])
+                    {
+                        minElement = matr[i, j];
+                    }
+                }
+            }
+            return minElement;
+        }
+
+        static int SearchCountMinElement(int[,] matr, int n, int m)
+        {
+            int minElement = SearchMinElement(matr, n, m), countMassIndexes = 0, i, j;
+            for (i = 0; i < n; i++)
+            {
+                for (j = 0; j < m; j++)
+                {
+                    if (minElement == matr[i, j])
+                    {
+                        countMassIndexes++;
+                    }
+                }
+            }
+            return countMassIndexes;
+        }
+
+        static int[,] SearchIndexesMinElementMatr(int[,] matr, int n, int m)
+        {
+            int i, j, k = -1, minElement = SearchMinElement(matr, n, m), countMassIndexes = SearchCountMinElement(matr, m, n);
+
+            int[,] massIndexesMinElementMatr = new int[countMassIndexes, 2];
+
+            for (i = 0; i < n; i++)
+            {
+                for (j = 0; j < m; j++)
+                {
+                    if (minElement == matr[i, j])
+                    {
+                        k++;
+                        massIndexesMinElementMatr[k, 0] = i+1;
+                        massIndexesMinElementMatr[k, 1] = j+1;
+                    }
+                }
+            }
+
+            return massIndexesMinElementMatr;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Random random = new Random();
+
+            int n, m, i, j, sumElement;
+            Console.Write("Введіть кількість стовпців: ");
+            n = int.Parse(Console.ReadLine());
+            Console.Write("Введіть кількість рядків: ");
+            m = int.Parse(Console.ReadLine());
+
+            int[,] matr = new int[n, m];
+
+            for (i = 0; i < n; i++)
+            {
+                for (j = 0; j < m; j++)
+                {
+                    matr[i, j] = random.Next(1, 10);
+                }
+            }
+
+            for (i = 0; i < n; i++)
+            {
+                for (j = 0; j < m; j++)
+                {
+                    Console.Write(matr[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            sumElement = SumElementsMatrAboveMainLine(matr, n, m);
+            Console.WriteLine($"Сума елементів матриці \nнад головною діагоналлю дорівнює: {sumElement}");
+
+            Console.WriteLine();
+
+            int[,] massIndexesMinElementMatr;
+
+            massIndexesMinElementMatr = SearchIndexesMinElementMatr(matr, n, m);
+            int minElement = SearchMinElement(matr, n, m), countMassIndexes = SearchCountMinElement(matr, n, m);
+
+            Console.WriteLine($"Найменший елемент в матриці: {minElement}");
+            Console.WriteLine("Індекси найменшого елементу в матриці");
+            for (i = 0; i < countMassIndexes; i++)
+            {
+                for (j = 0; j < 2; j++)
+                {
+                    Console.Write(massIndexesMinElementMatr[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
